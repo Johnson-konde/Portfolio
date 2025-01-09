87014d1dc2b454c02a3a7d7a20be6d7c329b675d
Johnson-konde <kondejohnson1@gmail.com>
2025-01-09 17:20:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+//document.querySelectorAll(".about").addEventListener('mouseenter', function() {
+  //  hoverLink.style.color = 'white'; // Color on hover
+  //});
+
+ // document.querySelectorAll(".about").addEventListener('mouseleave', function() {
+  //hoverLink.style.color = 'blue'; // Original color
+//});
